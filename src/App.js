fd8d9d4eec0caec4692a62290fdea973a7163ef1
kumbajirajkumar123 <raj.kumbaji3707@gmail.com>
2022-11-25 11:28:59 +0530

    jozbiz assignment
    uploading files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/navbar';
+import BodySection1 from './components/BodySection/BodySection1';
+import BodyBackground from './components/BodyBackground/BodyBackground';
+import BodySection2 from "./components/BodySection/BodySection2";
+import BodySection3 from './components/BodySection/BodySection3';
+import BodySection4 from './components/BodySection/BodySection4';
+import BodySection5 from './components/BodySection/BodySection5';
+import BodySection6 from './components/BodySection/BodySection6';
+import Footer from './components/Footer/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Navbar/>
+      <BodyBackground/>
+      <BodySection1/> 
+      <BodySection2/> 
+      <BodySection3/> 
+      <BodySection4/> 
+      <BodySection5/> 
+      <BodySection6/> 
+      <Footer />
+    </main>
   );
 }
 
